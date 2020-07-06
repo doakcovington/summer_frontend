864e7b6db0cba564d0e22525092ca1dedcc63f76
doakcovington <doakcovington@protonmail.com>
2020-07-06 15:47:30 -0700

    Built out getPosts function

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ console.log("We Don't Go To Ravenholm")
 const endPoint = "http://localhost:3000/api/v1/posts"
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch(endPoint)
-      .then(res => res.json())
-      .then(json => console.log(json));
+    getPosts();
   });
+
+function getPosts() {
+    fetch(endPoint)
+    .then(res => res.json())
+    .then(posts => { 
+        posts.data.forEach(post => {
+            const postMarkup = `
+                <div data-id=${post.id}>
+                    <h3>${post.attributes.title}</h3>
+                    <img src=${post.attributes.image_url}>
+                    <p>${post.attributes.caption}</p>
+                </div>
+            `; //end of postMarkup
+            document.querySelector('#post-container').innerHTML += postMarkup;
+        })
+    });
+}
   
