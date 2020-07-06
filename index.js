0c89bfd1bd9e76c2d2c263b2245a0a3cba1ce9d6
doakcovington <doakcovington@protonmail.com>
2020-07-06 15:33:43 -0700

    Built out DOM loaded event listener

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// http://localhost:3000/api/v1/posts
 console.log("We Don't Go To Ravenholm")
+
+const endPoint = "http://localhost:3000/api/v1/posts"
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(endPoint)
+      .then(res => res.json())
+      .then(json => console.log(json));
+  });
+  
